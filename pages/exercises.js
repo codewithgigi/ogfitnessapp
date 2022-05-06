4578e640ea6a6e37f002b1ad82fccd8480ed60e6
Jiji <jijigardi@gmail.com>
2022-05-05 18:09:01 -0700

    import exercises add limit to check all

--- pages/exercises.js
+++ pages/exercises.js
@@ -2,20 +2,28 @@ import Section from "../components/Section";
 import React, { useEffect, useState } from "react";
 import { API } from "aws-amplify";
 import { ExerciseList } from "../src/exerciselist";
+import { Glutes } from "../src/glutes";
 import { createExercise } from "../src/graphql/mutations";
 import { listExercises } from "../src/graphql/queries";
 
 export default function Exercises() {
   const [exercises, setExercises] = useState();
+  const [groupedExercises, setGroupedExercises] = useState();
 
   useEffect(() => {
     getExerciseList();
+    groupBy();
   }, []);
+
+  useEffect(() => {
+    groupBy();
+  }, [exercises]);
 
   async function getExerciseList() {
     try {
       const { data } = await API.graphql({
         query: listExercises,
+        variables: { limit: 300 },
         authMode: "AMAZON_COGNITO_USER_POOLS",
       });
       const items = data?.listExercises?.items;
@@ -25,12 +33,14 @@ export default function Exercises() {
     }
   }
   async function importExercises() {
-    ExerciseList.map(async (x) => {
-      const dataArray = x.split(",");
+    ExerciseList.map(async (e) => {
+      let exists;
+      const dataArray = e.split(",");
       const name = dataArray[1].toLowerCase();
-      const exists = exercises.find((x) => {
-        const exname = x?.exercise?.name;
-        if (exname && exname.toLowerCase() === name) return x;
+      exists = exercises.find((x) => {
+        if (x?.exercise?.name.toLowerCase() === name) {
+          return x;
+        }
       });
       if (!exists) {
         try {
@@ -57,12 +67,36 @@ export default function Exercises() {
       }
     });
   }
+  function groupBy() {
+    const byMuscle = (exercises ?? []).reduce((acc, obj) => {
+      const key = obj.exercise.muscles;
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+      // Add object to list for given key's value
+      acc[key].push(obj);
+      return acc;
+    }, {});
+    setGroupedExercises(byMuscle);
+  }
+  if (exercises) console.log(exercises.length);
+  const exerciseGroups = groupedExercises ? Object.keys(groupedExercises) : [];
   return (
     <Section>
-      <h1>Exercises</h1>
-      {(exercises ?? []).map((x, index) => (
-        <div key={index}>{x?.exercise?.name}</div>
-      ))}
+      <h1>Exercise List</h1>
+      <button onClick={() => importExercises()}>Import</button>
+      {(exerciseGroups ?? []).map((x) => {
+        return (
+          <div key={x}>
+            <h2>{x}</h2>
+            {(groupedExercises[x] ?? []).map((e, index) => (
+              <div key={index} style={{ textTransform: "capitalize" }}>
+                {e?.exercise?.name}
+              </div>
+            ))}
+          </div>
+        );
+      })}
     </Section>
   );
 }
---
+++ src/glutes.js
@@ -0,0 +1,14 @@
+//Data structure:
+//{muscles, name, level, bodypart, pp, modality, joint, ..... add these: description, instructions, video, image, id}
+
+export const Glutes = [
+  "Glutes,Bodyweight glute bridge,Beginner,Lower,Pull,FW,M",
+  "Glutes,Dumbell glute bridge,Intermediate,Lower,Pull,FW,M",
+  "Glutes,Barbell glute bridge,Advanced,Lower,Pull,FW,M",
+  "Glutes,single leg glute bridge,Advanced,Lower,Pull,FW,M",
+  "Glutes,Bodyweight hip thrust,Beginner,Lower,Pull,FW,M",
+  "Glutes,Dumbell hip thrust,Intermediate,Lower,Pull,FW,M",
+  "Glutes,Dumbell b-stance hip thrust,Intermediate,Lower,Pull,FW,M",
+  "Glutes,Barbell hip thrust,Advanced,Lower,Pull,FW,M",
+  "Glutes,Barbell b-stance hip thrust,Advanced,Lower,Pull,FW,M",
+];
