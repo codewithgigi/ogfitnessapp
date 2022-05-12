b1a5763c48a5527e3df104547ce9a4e0becb88fa
Jiji <jijigardi@gmail.com>
2022-05-11 22:08:45 -0700

    backend api workout workoutplan

--- pages/exercises.js
+++ pages/exercises.js
@@ -90,19 +90,17 @@ export default function Exercises() {
               return (
                 <div key={index} style={{ textTransform: "capitalize" }}>
                   {e?.exercise?.name}
+                  {e?.exercise?.video}
                   {e?.exercise?.video && (
                     <div>
-                      <div style="padding:56.25% 0 0 0;position:relative;">
-                        <iframe
-                          src={`https://player.vimeo.com/video/${e?.excercise?.video}?h=453494dbbe&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`}
-                          frameborder="0"
-                          allow="autoplay; fullscreen; picture-in-picture"
-                          allowfullscreen
-                          style="position:absolute;top:0;left:0;width:100%;height:100%;"
-                          title="test-track.MOV"
-                        ></iframe>
-                      </div>
-                      <script src="https://player.vimeo.com/api/player.js"></script>
+                      <iframe
+                        width="100%"
+                        src="https://vimeo.com/707912022"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        title="OGFit.app welcome screen"
+                      />
                     </div>
                   )}
                   {e?.exercise.image && (
